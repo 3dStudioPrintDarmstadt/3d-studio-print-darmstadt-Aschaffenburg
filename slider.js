bffa6f92fc763b01b47b57cef455f2f763261619
3dStudioPrintDarmstadt <3dstudiodarmstadt@gmail.com>
2025-12-09 06:03:27 +0100

    Update slider.js

--- slider.js
+++ slider.js
@@ -1,23 +1,20 @@
 let currentIndex = 0;
 const images = document.querySelectorAll('.portfolio-slider img');
-const prevButton = document.querySelector('.prev');
-const nextButton = document.querySelector('.next');
 
 function showImage(index) {
-  images.forEach((image, i) => {
-    image.style.display = i === index ? 'block' : 'none';  // Zeigt nur das aktuelle Bild an
+  images.forEach((img, i) => {
+    img.style.display = i === index ? 'block' : 'none';
   });
 }
 
-prevButton.addEventListener('click', () => {
+document.querySelector('.prev').onclick = () => {
   currentIndex = (currentIndex === 0) ? images.length - 1 : currentIndex - 1;
   showImage(currentIndex);
-});
+};
 
-nextButton.addEventListener('click', () => {
+document.querySelector('.next').onclick = () => {
   currentIndex = (currentIndex === images.length - 1) ? 0 : currentIndex + 1;
   showImage(currentIndex);
-});
+};
 
-// Zeigt das erste Bild an
 showImage(currentIndex);
